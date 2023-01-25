43bed1b5e05bc619a021bfd48f2eff4c575231e5
Robles <joseroblesarce@yahoo.es>
2023-01-25 10:35:12 +0000

    ejercicio practico coche1

---
+++ src/coche.java
@@ -0,0 +1,15 @@
+public class coche{
+    int numeroP=0;
+    coche(){
+
+
+    }
+    public void agregarP(){
+        numeroP++;
+
+    }
+    public void mostrarP() {
+        System.out.println("La cantidad de puertas es:"+numeroP);
+    }
+
+}
